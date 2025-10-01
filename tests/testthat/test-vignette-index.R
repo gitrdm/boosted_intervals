402cb979149399c4790dbd14b60f42f02f5aae00
gitrdm <49376741+gitrdm@users.noreply.github.com>
2025-10-01 16:07:44 -0500

    Added fermi-pizza viginette

--- tests/testthat/test-vignette-index.R
+++
@@ -1,8 +0,0 @@
-context("vignette index renders")
-
-test_that("vignette index renders to HTML", {
-  skip_on_cran()
-  pkgload::load_all()
-  out <- rmarkdown::render("vignettes/index.Rmd", output_format = "html_vignette", quiet = TRUE)
-  expect_true(file.exists(out))
-})
